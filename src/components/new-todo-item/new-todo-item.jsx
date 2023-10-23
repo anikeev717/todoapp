fbce2e4ebecaecf1b7aeb406f318e4e5f1ec00d6
anikeev717 <anikeev717@gmail.com>
2023-10-23 16:37:27 +0300

    add timer feature

--- src/components/new-todo-item/new-todo-item.jsx
+++ src/components/new-todo-item/new-todo-item.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './new-todo-item.css';
 
 export class NewTodoItem extends Component {
-  state = { label: '' };
+  state = { label: '', time: { min: '', sec: '' } };
 
   onLabelChange = (e) => {
     this.setState({
@@ -12,24 +12,71 @@ export class NewTodoItem extends Component {
     });
   };
 
+  onTimerChange = (e, timeUnit) => {
+    const { time: oldTime } = this.state;
+    const newTime = JSON.parse(JSON.stringify(oldTime));
+    const reg = /^0+|(^6[1-9])|(^[7-9]\d)|[^\d]/gm;
+    // const reg = timeUnit === 'min' ? /^0+|(^6[1-9])|(^[7-9]\d)|[^\d]/gm : /^0+|(^[6-9]\d)|[^\d]/gm;
+    const newTimeValue = e.target.value.replace(reg, '').slice(0, 2);
+    newTime[timeUnit] = newTimeValue;
+    this.setState({
+      time: newTime,
+    });
+  };
+
   onSubmit = (e) => {
-    const { label } = this.state;
+    const {
+      label,
+      time: { min, sec },
+    } = this.state;
     const { onItemAdded } = this.props;
     const newLabel = label.replace(/\s+/g, ' ').trim();
+    const newTime = Number(min) * 60 + Number(sec);
     e.preventDefault();
-    if (newLabel) {
-      onItemAdded(newLabel);
-      this.setState({ label: '' });
+    if (newLabel && newTime) {
+      onItemAdded(newLabel, newTime);
+      this.setState({ label: '', time: { min: '', sec: '' } });
     }
   };
 
+  onKeyEnter = (e) => {
+    if (e.key === 'Enter') this.onSubmit(e);
+  };
+
   render() {
-    const { label } = this.state;
+    const {
+      label,
+      time: { min, sec },
+    } = this.state;
 
     return (
       <form className="header" onSubmit={this.onSubmit}>
         <h1>todos</h1>
-        <input className="new-todo" placeholder="What needs to be done?" onChange={this.onLabelChange} value={label} />
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          onKeyDown={this.onKeyEnter}
+          onChange={this.onLabelChange}
+          value={label}
+        />
+        <input
+          className="new-todo new-todo-timer"
+          placeholder="Min"
+          onKeyDown={this.onKeyEnter}
+          onChange={(e) => {
+            this.onTimerChange(e, 'min');
+          }}
+          value={min}
+        />
+        <input
+          className="new-todo new-todo-timer"
+          placeholder="Sec"
+          onKeyDown={this.onKeyEnter}
+          onChange={(e) => {
+            this.onTimerChange(e, 'sec');
+          }}
+          value={sec}
+        />
       </form>
     );
   }
